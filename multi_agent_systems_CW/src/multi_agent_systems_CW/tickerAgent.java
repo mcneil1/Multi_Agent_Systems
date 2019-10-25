f0ef7a249c95f988339410ed76308ffcd1912e91
Robbie McNeil <robbiemnc98@gmail.com>
2019-10-25 13:30:40 +0100

    Initial commit, ticker agent added

---
+++ multi_agent_systems_CW/src/multi_agent_systems_CW/tickerAgent.java
@@ -0,0 +1,18 @@
+package multi_agent_systems_CW;
+
+import java.util.ArrayList;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class tickerAgent 
+{
+	public static final int NUM_DAYS = 100;
+}
